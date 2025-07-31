859e2e797a7044ddf4478807204413713d6c6519
Nayan <n007kingkong@gmail.com>
2025-07-31 23:12:36 +0600

    object value accessing

---
+++ object.js
@@ -0,0 +1,25 @@
+
+
+const bottle = {
+    brand : 'apple',
+    price : 45,
+    color : 'white',
+    isClean : true
+
+}
+
+// console.log(bottle);
+console.log(bottle.color);
+
+
+// const animal = {
+//     cat : {
+//         name:'mjdmbd',
+//         age : 25,
+//         color: 'black'
+//     },
+//     tiger : 5,
+//     lion : 10
+// }
+
+// console.log(animal);
