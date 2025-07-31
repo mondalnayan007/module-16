d890003183d43bb510fee33646c861a2e852aae7
Nayan <n007kingkong@gmail.com>
2025-07-31 22:12:37 +0600

    problem solved

---
+++ string-compare.js
@@ -0,0 +1,16 @@
+
+
+
+const savedMail = 'nayan210091@gmail.com';
+
+const enteredMail = 'NAYAN210091@GMAIL.COM';
+
+if(savedMail === enteredMail.toLowerCase() ){
+    console.log("you can login");
+
+}
+else{
+    console.log(enteredMail);
+     console.log("Vagggg sala  !!!!!!!!!!");
+} 
+
